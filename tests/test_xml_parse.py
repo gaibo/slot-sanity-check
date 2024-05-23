14d404e3cedab5a055e47c49222051a7294d77de
Gaibo Zhang <gaibozhang@yahoo.com>
2024-05-23 16:50:13 -0400

    learn more about writing tests; add LICENSE (wow, I've never tried that before)

--- tests/test_xml_parse.py
+++ tests/test_xml_parse.py
@@ -15,34 +15,42 @@ from utils import xml_dfs_get_text_set
 
 
 class TestXMLParse(unittest.TestCase):
-
-    def test_understanding(self):
-        # Load XML example file/tree/root
+    def setUp(self) -> None:
+        # Load XML example file/tree/root (ui_chara_db)
         mod_path = Path(r'Bowser (C05) - Swole Dedede')
         readable_param_patch_ui_chara_db = list(mod_path.glob('**/ui_chara_db.prcxml'))
         ui_chara_db_tree = ET.parse(readable_param_patch_ui_chara_db[0])
-        struct_root = ui_chara_db_tree.getroot()  # <struct> opening tag
+        self.struct_root_swole = ui_chara_db_tree.getroot()  # <struct> opening tag
+        # Load another XML example file/tree/root (msg_name)
+        readable_param_patch_msg_name = list(mod_path.glob('**/msg_name.xmsbt'))
+        msg_name_tree = ET.parse(readable_param_patch_msg_name[0])
+        self.xmsbt_root_swole = msg_name_tree.getroot()  # <xmsbt> opening tag
 
+    def test_understanding(self) -> None:
         # Assert understanding of XML tools: tag, attrib, text (already did this above)
-        list_node = list(struct_root)[0]    # Specific "list" 
-        self.assertTrue(list_node.tag == 'list')
+        list_node = list(self.struct_root_swole)[0]    # Always a <list> tag in context of these mods
+        self.assertEqual(list_node.tag, 'list')
         modified_indexes = [child for child in list_node if child.tag == 'struct']
         mod_idx = modified_indexes[0]
         mod_lines = list(mod_idx)
-        self.assertTrue(mod_lines[0].tag == 'byte')
-        self.assertTrue(mod_lines[0].attrib['hash'] == 'n05_index')
-        self.assertTrue(mod_lines[0].text == '13')
+        with self.subTest(i=1):
+            self.assertEqual(mod_lines[0].tag, 'byte')
+            self.assertEqual(mod_lines[0].attrib['hash'], 'n05_index')
+            self.assertEqual(mod_lines[0].text, '13')
+        with self.subTest(i=2):
+            self.assertEqual(mod_lines[1].tag, 'hash40')
+            self.assertEqual(mod_lines[1].attrib['hash'], 'characall_label_c13')
+            self.assertEqual(mod_lines[1].text, 'vc_narration_characall_koopa')
     
-    def test_dfs(self):
-        # Load XML example file/tree/root
-        mod_path = Path(r'Bowser (C05) - Swole Dedede')
-        readable_param_patch_ui_chara_db = list(mod_path.glob('**/ui_chara_db.prcxml'))
-        ui_chara_db_tree = ET.parse(readable_param_patch_ui_chara_db[0])
-        struct_root = ui_chara_db_tree.getroot()  # <struct> opening tag
-        
+    def test_DFS_for_text(self) -> None:        
         # Do a DFS for interesting stuff; in this case, look for "text" (mostly inside structs)
-        text_set = xml_dfs_get_text_set(struct_root)
-        self.assertTrue('vc_narration_characall_koopa' in text_set)
+        struct_text_set = xml_dfs_get_text_set(self.struct_root_swole)
+        self.assertTrue('vc_narration_characall_koopa' in struct_text_set)
+        self.assertTrue('dummy' in struct_text_set)
+        xmsbt_text_set = xml_dfs_get_text_set(self.xmsbt_root_swole)
+        self.assertTrue('Swole Dedede' in xmsbt_text_set)
+        self.assertTrue('SWOLE DEDEDE' in xmsbt_text_set)
+        self.assertTrue('The Ripped\nPenguin' in xmsbt_text_set)
 
 
 if __name__ == '__main__':
